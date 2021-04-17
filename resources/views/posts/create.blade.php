18a6291daaaddd793b589055d37af1ceee53018e
Viet Nguyen <vietnh90@gmail.com>
2021-04-17 16:58:05 +0700

    Using https

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <title>Noithat c3</title>
     <meta name="theme-color" content="#fafafa">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
